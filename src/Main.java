4fc394aa118e1f0d4e6cf3a2a94c8e0268f4db06
Lucas Jardim <lucas.pasqualijardim59@gmail.com>
2024-09-11 15:22:02 -0300

    Calculo das notas

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        double nota1 = 8.5;
+        double nota2 = 7.0;
+        double nota3 = 9.2;
+        double nota4 = 6.8;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        System.out.println("Média das notas é: " + media);
         }
     }
-}
