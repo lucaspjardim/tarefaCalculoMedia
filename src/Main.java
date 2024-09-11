8d54c23278884d5fd5430033d1fdc866dd3ad6cb
Lucas Jardim <lucas.pasqualijardim59@gmail.com>
2024-09-11 15:23:40 -0300

    Criando JavaDoc

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/**
+ * A classe Main realiza o cálculo da média de quatro notas predefinidas.
+ *
+ * <p>
+ * O programa define quatro notas diretamente no código, calcula a média
+ * divindo por 4.
+ * </p>
+ *
+ * @author Lucas Jardim
+ */
 public class Main {
+
+    /**
+     * O método principal {@code main} faz o cálculo da média das quatro notas
+     * e exibe o resultado no console.
+     *
+     * @param args Argumentos da linha de comando (não utilizados neste exemplo).
+     */
     public static void main(String[] args) {
         double nota1 = 8.5;
         double nota2 = 7.0;
         double nota3 = 9.2;
         double nota4 = 6.8;
 
+        // Cálculo da média
         double media = (nota1 + nota2 + nota3 + nota4) / 4;
 
+        // Exibição da média no console
         System.out.println("Média das notas é: " + media);
-        }
     }
+}
